81c687ccdb666922749fc92245e4c966396eba66
정유진 <jane320@naver.com>
2023-10-20 12:59:04 +0900

    영화 카드 생성 및  추가 && 필터링 기능 추가

--- search.js
+++ search.js
@@ -1,11 +1,38 @@
 const form = document.querySelector('.search');
+const searchInput = document.querySelector('#search__input');
+const searchBtn = document.querySelector('.search__btn');
 const movieLists = document.querySelector('.movie-lists')
-
+let movies;
 
 function onSubmit(e) {
     e.preventDefault();
+    if (searchInput.value.trim() !== '') filterMovieCard();
 }
 
+
+
+function showMovieCard(movies) {
+    movies.map(movie => {
+        const movieCard = addMovieCard(movie);
+        movieLists.appendChild(movieCard);
+        movieCard.addEventListener('click', () => {
+            alert(`영화 id : ${movie.id}`);
+        })
+    });
+
+}
+
+function filterMovieCard() {
+    //input에서 입력한 값을 가져오고 소문자로 모두 변환함. 
+    const keyword = searchInput.value.toLowerCase();
+    //영화 데이터들 중에서 제목이 해당 키워드를 포함한 영화만 보여주고 그렇지 않으면 숨기기
+    movies.map((movie) => {
+        if (!movie.title.toLowerCase().includes(keyword)) document.getElementById(`${movie.id}`).style.display = 'none';
+        else document.getElementById(`${movie.id}`).style.display = 'block';
+    })
+}
+
+//해당 영역에만 무비 카드를 만듬. 전체면 전체고, 필터링이면 필터링만 만들기 
 function addMovieCard(movie) {
     let { id, title, overview, poster_path, vote_average } = movie;
     poster_path = `https://image.tmdb.org/t/p/original/${poster_path}`;
@@ -21,6 +48,7 @@ function addMovieCard(movie) {
     poster.setAttribute('src', poster_path);
 
     const content = document.createElement('p');
+    content.setAttribute('class', 'content')
     content.textContent = overview;
 
     const score = document.createElement('p');
@@ -34,29 +62,38 @@ function addMovieCard(movie) {
     return card;
 }
 
-async function getMovieList() {
+
+
+async function getMovies() {
     const options = {
-        method: 'GET',
         headers: {
             accept: 'application/json',
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiODRhYTdhYzI0YzA4MDQ5NTliZTYwMWQ1NGNiNTVmNSIsInN1YiI6IjY1MzA3NDg4OWQ1OTJjMDBlY2NhNDIxMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.pzeR-WgSZE9rH2RlMjPFsuTfxUsjdYUGJY41msWPjyE'
+            Authorization: `Bearer ${config.key}`
         }
     };
+    const url = config.url;
 
-    fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
-        .then(res => { return res.json() })
-        .then(data => {
-            const movies = data.results;
-            console.log(data);
-            movies.map(movie => {
-                const movieCard = addMovieCard(movie);
-                movieLists.appendChild(movieCard);
-            })
+    await axios.get(url, options)
+        .then(res => {
+            movies = res.data.results;
+            showMovieCard(movies);
         })
-        .catch(err => console.error(err));
+        .catch(console.err);
+
+
+
+    // fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
+    //     .then(res => { return res.json() })
+    //     .then(data => {
+    //         const movies = data.results;
+    //         //영화들 데이터를 가져와 영화 카드를 보여줌. 
+    //         showMovieCard(movies);
+    //     })
+    //     .catch(err => console.error(err));
+
 }
 
-getMovieList();
 
 
 form.addEventListener('submit', onSubmit);
+window.addEventListener('load', getMovies)
